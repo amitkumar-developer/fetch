75b8bb2c2ffe4a5238170e965d04acba478f0d13
amitkumar-developer <itsamitkumar03@gmail.com>
2023-06-18 20:18:11 +0530

    route-page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 import Posts from './Components/Posts';
 import Comments from './Components/Comments';
 import Todo from './Components/Todo';
@@ -12,15 +13,37 @@ const App = () => {
   return (
     <div>
       
+  
+
+
+<BrowserRouter>
+      <Link to="/Posts" >Posts</Link><br></br>
+      <Link to="/Comments" >Comments</Link><br></br>
+      <Link to="/Todo" >Todos</Link><br></br>
+      <Link to="/Photos" >Photos</Link><br></br>
+      <Link to="/Albums" >Albums</Link><br></br>
+
+
+      <Routes>
+           <Route path="/posts" element={<Posts/>}/>
+           <Route path="/comments" element={<Comments/>}/>
+           <Route path="/todo" element={<Todo/>}/>
+           <Route path="/photos" element={<Photos/>}/>
+           <Route path="/albums" element={<Albums/>}/>
+
+
+
+      </Routes>
+      </BrowserRouter>
     
 
          
 
-      <Posts />
+      {/* <Posts />
       <Comments />
       <Todo />
       <Photos/>
-      <Albums/>
+      <Albums/> */}
 
 
 
